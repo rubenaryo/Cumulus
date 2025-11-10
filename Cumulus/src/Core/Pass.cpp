a4386c2635667f20373cb324d3f192c37f754614
Ruben Young <40431675+rubenaryo@users.noreply.github.com>
2025-11-10 11:29:18 -0500

    Bind any samplers from fixed list (#31)
    We define 6 static samplers:
    pointWrap,
    pointClamp,
    linearWrap,
    linearClamp,
    anisotropicWrap,
    anisotropicClamp
    
    The indices are fixed and everytime a shader asks for one, that one will be provided by the engine.

--- Cumulus/src/Core/Pass.cpp
+++ Cumulus/src/Core/Pass.cpp
@@ -90,22 +90,7 @@ bool Pass::GenerateRootSignature()
 
     for (const auto& sampler : Samplers)
     {
-        D3D12_STATIC_SAMPLER_DESC samplerDesc = {};
-        samplerDesc.Filter = D3D12_FILTER_MIN_MAG_MIP_LINEAR;
-        samplerDesc.AddressU = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
-        samplerDesc.AddressV = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
-        samplerDesc.AddressW = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
-        samplerDesc.MipLODBias = 0;
-        samplerDesc.MaxAnisotropy = 16;
-        samplerDesc.ComparisonFunc = D3D12_COMPARISON_FUNC_LESS_EQUAL;
-        samplerDesc.BorderColor = D3D12_STATIC_BORDER_COLOR_OPAQUE_WHITE;
-        samplerDesc.MinLOD = 0.0f;
-        samplerDesc.MaxLOD = D3D12_FLOAT32_MAX;
-        samplerDesc.ShaderRegister = sampler.BindPoint;
-        samplerDesc.RegisterSpace = sampler.Space;
-        samplerDesc.ShaderVisibility = sampler.Visibility;
-
-        builder.AddStaticSampler(samplerDesc);
+        builder.AddStaticSampler(sampler.BindPoint, sampler.Space);
     }
 
     return builder.Build(pDevice, mpRootSignature.GetAddressOf());
