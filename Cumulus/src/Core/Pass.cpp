1c650bc96064db9d457ef917357aaf33ee45d456
Ser-Avi <128562182+Ser-Avi@users.noreply.github.com>
2025-11-14 15:10:55 -0500

    Cleanup, added comments, sphere render is now a macro

--- Cumulus/src/Core/Pass.cpp
+++ Cumulus/src/Core/Pass.cpp
@@ -92,13 +92,13 @@ bool Pass::GenerateRootSignature()
     {
         D3D12_STATIC_SAMPLER_DESC samplerDesc = {};
         samplerDesc.Filter = D3D12_FILTER_MIN_MAG_MIP_LINEAR;
-        samplerDesc.AddressU = D3D12_TEXTURE_ADDRESS_MODE_CLAMP;
-        samplerDesc.AddressV = D3D12_TEXTURE_ADDRESS_MODE_CLAMP;
-        samplerDesc.AddressW = D3D12_TEXTURE_ADDRESS_MODE_CLAMP;
+        samplerDesc.AddressU = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
+        samplerDesc.AddressV = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
+        samplerDesc.AddressW = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
         samplerDesc.MipLODBias = 0;
         samplerDesc.MaxAnisotropy = 16;
         samplerDesc.ComparisonFunc = D3D12_COMPARISON_FUNC_LESS_EQUAL;
-        samplerDesc.BorderColor = D3D12_STATIC_BORDER_COLOR_OPAQUE_BLACK;
+        samplerDesc.BorderColor = D3D12_STATIC_BORDER_COLOR_OPAQUE_WHITE;
         samplerDesc.MinLOD = 0.0f;
         samplerDesc.MaxLOD = D3D12_FLOAT32_MAX;
         samplerDesc.ShaderRegister = sampler.BindPoint;
