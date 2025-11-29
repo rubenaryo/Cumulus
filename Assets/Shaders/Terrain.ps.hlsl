d017af09e44b026c1f49eea0f092374f1c27eacd
rubenaryo <rubeny484@gmail.com>
2025-11-29 06:31:36 -0500

    Add terrain pass and new shaders

---
+++ Assets/Shaders/Terrain.ps.hlsl
@@ -0,0 +1,53 @@
+#include "PhongCommon.hlsli"
+
+struct VertexOut
+{
+    float4 position : SV_POSITION;
+    float3 normal : NORMAL;
+    float2 uv : TEXCOORD;
+    float3 worldPos : POSITION;
+};
+
+cbuffer PSLights : register(b10)
+{
+    float3 ambientColor;
+    DirectionalLight directionalLight;
+    float3 cameraWorldPos;
+}
+
+cbuffer PSPerMaterial : register(b11)
+{
+    float4 colorTint;
+    float specularity;
+}
+
+Texture2D diffuseTexture : register(t0);
+Texture3D test3d : register(t1);
+SamplerState samplerOptions : register(s0);
+
+float4 main(VertexOut input) : SV_TARGET
+{
+    // Sample diffuse texture
+    float3 surfaceColor = diffuseTexture.Sample(samplerOptions, input.uv).rgb;
+    
+    // Normalize normal vector
+    input.normal = normalize(input.normal);
+    
+    // Holds the total light for this pixel
+    float3 totalLight = 0;
+    float3 toCamera = normalize(cameraWorldPos - input.worldPos);
+    
+    // Diffuse Color
+    float3 diffuseLighting = directionalLight.diffuseColor.rgb *
+        DiffuseAmount(input.normal, normalize(directionalLight.toLight));
+    
+    // Add to totallight
+    totalLight += diffuseLighting;
+    
+    // Finally, add the ambient color
+    totalLight += ambientColor;
+    
+    totalLight *= surfaceColor;
+    
+    return float4(totalLight, 1);
+}
